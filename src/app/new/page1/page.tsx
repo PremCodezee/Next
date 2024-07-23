7e49b9f59ae12e26bb161d1407aad9bf28a9d4c6
Prem Codezee <prem.codezee.gmail.com>
2024-07-23 16:13:33 +0530

    Basics

---
+++ src/app/new/page1/page.tsx
@@ -0,0 +1,9 @@
+export default function pageTwo () {
+
+    return (
+
+        <h1 className="text-2xl text-center p-5 bg-slate-800 text-green-400 font-bold">
+            page two
+        </h1>
+    )
+}
